77a533a4794973f717e1002eadf54fc1f59aae37
misterboe <me@bschauer.de>
2016-05-11 11:16:27 +0200

    Update ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -5,7 +5,7 @@ if (!defined('TYPO3_MODE')) {
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,                    // Extension Key
-    'Configuration/TypoScript',  // Path to setup.txt and constants.txt
-    'Example Package'            // Title in the selector box
+    $_EXTKEY,
+    'Configuration/TypoScript',
+    'Typo3 Theme'
 );
